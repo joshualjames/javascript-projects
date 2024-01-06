3023d6dd6f50404e7bacba954e0b76959258bd7c
Joshua James <josh452063@gmail.com>
2024-01-06 11:27:33 -0600

    <placed code in between prompts

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,12 @@
 // Declare and assign the variables below
 
-
 const shuttle = "Determination";
 const speed = Number(17500);
 const marsDistKm = Number(225000000)
 const moonDistKm = Number(394400)
 const miPerKm = Number(.621)
+
+// Use console.log to print the 'typeof' each variable. Print one item per line.
 
 console.log(typeof shuttle)
 console.log(typeof speed)
@@ -13,28 +14,35 @@ console.log(typeof marsDistKm)
 console.log(typeof moonDistKm)
 console.log(typeof miPerKm)
 
+// Calculate a space mission below
+
 let marsDistMi = (marsDistKm * miPerKm)
 
 let hrsToMars = marsDistMi / speed
 
 let daysToMars = hrsToMars / 24 
 
+// Print the results of the space mission calculations below
+
 console.log(shuttle + " will take " + daysToMars + " days to reach Mars")
+
+// Calculate a trip to the moon below
 
 let moonDistMi = moonDistKm * miPerKm
 let hrsToMoon = moonDistMi / speed
 let daysToMoon = hrsToMoon / 24
 
+// Print the results of the trip to the moon below
+
 console.log(shuttle + " will take " + daysToMoon + " days to reach the moon")
 
 
 
-// Use console.log to print the 'typeof' each variable. Print one item per line.
 
-// Calculate a space mission below
 
-// Print the results of the space mission calculations below
 
-// Calculate a trip to the moon below
 
-// Print the results of the trip to the moon below
+
+
+
+
