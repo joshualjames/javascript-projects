dadd0e52b495829bbec2ef687760dc2bad22e6b9
Joshua James <josh452063@gmail.com>
2024-01-19 13:55:11 -0600

    completed.

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,9 @@
 // create an array variable containing the names
 
+let dndClasses = ["Cleric", "Bard", "Wizard", "Rogue"]
+
 // write a for loop that prints each name on a different line
+
+for (let i = 0; i < dndClasses.length; i++) {
+    console.log(dndClasses[i]);
+}
