5b812fdce2f47ef61fe107cb102e48f15641bcc5
Joshua James <josh452063@gmail.com>
2024-01-29 16:16:37 -0600

    practice your skills done. Raid a shuttle needs work.

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -6,10 +6,32 @@ b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
 
+// let tripleNum = function (num) {
+//     if (typeof (num) === Number) {
+//         return num * 3; 
+//     } else if (typeof (num) === String) {
+//         return "ARRR!"
+//     } else {
+//         return num
+//     }
+// }
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
-a) Triple any the numbers.
+a) Triple all of the numbers.
 b) Replace any strings with “ARRR!”
 c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+let tripleNum = function (num) {
+    if (typeof (num) === "number") {
+        return num * 3;
+    } else if (typeof (num) === "string") {
+        return "ARRR!"
+    } else {
+        return num
+    }
+}
+let trippled = arr.map(tripleNum);
+console.log(arr);
+console.log(trippled);
