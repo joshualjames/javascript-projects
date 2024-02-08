969fad344a9047f7b47e6e4ee1de5fe3d94ac733
Joshua James <josh452063@gmail.com>
2024-02-08 11:33:46 -0600

    mostly completed transmission example

---
+++ unit-testing/chapter-examples/transmission-processor/processor.js
@@ -0,0 +1,21 @@
+function processor(transmission) {
+    if (transmission.indexOf("::") < 0) {
+        // Data is invalid
+        return -1;
+    }
+    let parts = transmission.split("::");
+    let rawData = parts[1];
+    if (rawData[0] !== "<") {
+        rawData = -1;
+    };
+    if (rawData[rawData.length - 1] !== ">") {
+        rawData = -1;
+    }
+    return {
+        id: Number(parts[0]),
+        rawData: rawData
+    };
+    return {};
+}
+
+module.exports = processor;
