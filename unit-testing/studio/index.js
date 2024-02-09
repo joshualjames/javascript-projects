3ea71183bde0156e0662255dc2516d35ab02cbec
Joshua James <josh452063@gmail.com>
2024-02-09 12:10:58 -0600

    studio complete.

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,36 @@
 
 let launchcode = {
-  
-}
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["JavaScript", "Python", "Java"],
+    launchOutput: function launchOutput(num) {
+        if (num % 2 === 0 && num % 3 !== 0 && num % 5 !== 0) {
+            return 'Launch!';
+        }
+        if (num % 3 === 0 && num % 2 !== 0 && num % 5 !== 0) {
+            return 'Code!';
+        }
+        if (num % 5 === 0 && num % 2 !== 0 && num % 3 !== 0) {
+            return 'Rocks!';
+        }
+        if (num % 2 === 0 && num % 3 === 0 && num % 5 !== 0) {
+            return 'LaunchCode!';
+        }
+        if (num % 3 === 0 && num % 5 === 0 && num % 2 !== 0) {
+            return 'Code Rocks!';
+        }
+        if (num % 2 === 0 && num % 5 === 0 && num % 3 !== 0) {
+            return 'Launch Rocks! (CRASH!!!!)';
+        }
+        if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+            return 'LaunchCode Rocks!';
+        }
+        if (num % 2 !== 0 && num % 3 !== 0 && num % 5 !== 0) {
+            return "Rutabagas! That doesn't work.";
+        }
+    }
+};
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,78 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode", function () {
 
-  // Write your unit tests here!
+  test("verify the value of organization is 'nonprofit'", () => {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+
+  test("verify the value of executiveDirector is 'Jeff'", () => {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("verify the value of percentageCoolEmployees has the type number.", () => {
+    expect(typeof (launchcode.percentageCoolEmployees)).toBe("number");
+  });
   
+  test("verify 'JavaScript' is contained within the array of programsOffered", () => {
+    expect(launchcode.programsOffered.includes("JavaScript"));
+  });
+
+  test("verify 'Python' is contained within the array of programsOffered", () => {
+    expect(launchcode.programsOffered.includes("Python"));
+  });
+
+  test("verify 'Java' is contained within the array of programsOffered", () => {
+    expect(launchcode.programsOffered.includes("JavaScript"));
+  });
+
+  test("verify the length of the array within programsOffered.", () => {
+    expect(launchcode.programsOffered.length).toBe(3)
+  });
+
+});
+
+describe("Testing launchOutput", function () {
+  
+  test("verify a number divisible by 2 passed to launchOutput() returns 'Launch!'", () => {
+    let result = launchcode.launchOutput(8);
+    expect(result).toBe('Launch!');
+  });
+
+  test("verify a number only divisible by 3 passed to launchOutput() returns 'Code!'", () => {
+    let result = launchcode.launchOutput(9);
+    expect(result).toBe('Code!');
+  });
+
+  test("verify a number only divisible by 5 passed to laucnOutput() returns 'Rocks!'", () => {
+    let result = launchcode.launchOutput(25);
+    expect(result).toBe('Rocks!');
+  });
+
+  test("verify a number divisible by 2 and 3 passed to launchOutput() returns 'LaunchCode!'", () => {
+    let result = launchcode.launchOutput(12);
+    expect(result).toBe('LaunchCode!');
+  });
+
+  test("verify a number divisible by 3 and 5 passed to launchOutput() returns 'Code Rocks!'", () => {
+    let result = launchcode.launchOutput(15);
+    expect(result).toBe('Code Rocks!');
+  });
+
+  test("verify a number divisible by 2 and 5 passed to launchOutput() returns 'Launch Rocks!'", () => {
+    let result = launchcode.launchOutput(10);
+    expect(result).toBe('Launch Rocks! (CRASH!!!!)');
+  });
+
+  test("verify a number divisible by 2, 3, and 5 passed to launchOutput() returns 'LaunchCode Rocks!'", () => {
+    let result = launchcode.launchOutput(30);
+    expect(result).toBe('LaunchCode Rocks!');
+  });
+  
+  test("verify a number not divisible by 2, 3, or 5 passed to launchOutput() returns 'Rutabagas! That doesn't work.'", () => {
+    let result = launchcode.launchOutput(13);
+    expect(result).toBe("Rutabagas! That doesn't work.");
+  });
+
 });
