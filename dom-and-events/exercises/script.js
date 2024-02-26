8a8b8f4f15868e07d37ab95826cf63ced43e8bc2
Joshua James <josh452063@gmail.com>
2024-02-25 19:38:59 -0600

    completed exercises.

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,27 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", event => {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", event => {
+        event.target.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", event => {
+        event.target.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", event => {
+        result = window.confirm("Are you sure you want to abort the mission?");
+        if (result === true) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        // } else {
+            //cancel pressed;
+        };
+    });
+
 }
 
 window.addEventListener("load", init);
