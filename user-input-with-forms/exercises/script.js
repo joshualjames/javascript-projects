770525f60552a5416c99e5ac4b0d814e1d468251
Joshua James <josh452063@gmail.com>
2024-02-29 16:59:20 -0600

    completed exercise.

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,21 @@
 //Code Your Solution Below
+window.addEventListener("load", function () {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        let testNameInput = document.querySelector("input[name=testName]");
+        let testDateInput = document.querySelector("input[name=testDate]");
+        let rocketTypeInput = document.querySelector("input[name=rocketType]");
+        let boosterCountInput = document.querySelector("input[name=boosterCount]");
+        let windRatingInput = document.querySelector("input[name=windRating]");
+        let productionServersInput = document.querySelector("input[name=productionServers]");
+
+        if (testNameInput.value === "" ||
+            testDateInput.value === "" ||
+            rocketTypeInput.value === "" ||
+            boosterCountInput.value === "" ||
+            windRatingInput.value === "" ||
+            productionServersInput.value === "") {
+            alert("All fields are required!");
+        }
+    });
+});
